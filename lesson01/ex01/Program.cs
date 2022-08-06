8ebc62a81ad702b7dd6707dc0587cdf963dd65d4
«iliya <iliya.91f@gmail.com>
2022-08-06 17:55:37 +0300

    first task

--- lesson01/ex01/Program.cs
+++ lesson01/ex01/Program.cs
@@ -1,2 +1,12 @@
-﻿
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+
+System.Console.WriteLine("Please enter number");
+string number = Console.ReadLine();
+int x = Convert.ToInt32 (number);
+
+int result = x * x; 
+
+
+Console.WriteLine(result);
+
