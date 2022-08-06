4f8af746d61532e648c3370fb97a73d10f186d1f
«iliya <iliya.91f@gmail.com>
2022-08-06 22:01:44 +0300

    ex04

--- lesson01/ex04/Program.cs
+++ lesson01/ex04/Program.cs
@@ -1,12 +1,11 @@
 ﻿
-System.Console.WriteLine("Please enter number from 1 to 100");
+System.Console.WriteLine("Please enter number");
 int Number = Convert.ToInt32 (Console.ReadLine ()); 
+int NegativeNumber = (Number/Number) - (Number+1);
 
 
+while (Number != NegativeNumber)
+{
+Number--; 
+Console.WriteLine (Number);
 
-
-/*
-Напишите программу, которая на вход принимает одно число (N), а на выходе показывает все целые числа в промежутке от -N до N.
-4 -> "-4, -3, -2, -1, 0, 1, 2, 3, 4" 
-2 -> " -2, -1, 0, 1, 2"
-*/
