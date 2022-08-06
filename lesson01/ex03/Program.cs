b909dd94f1da3fd40820290fa70b9fdb84704880
«iliya <iliya.91f@gmail.com>
2022-08-06 18:46:10 +0300

    ex03

---
+++ lesson01/ex03/Program.cs
@@ -0,0 +1,38 @@
+﻿
+System.Console.WriteLine("Please enter number from 1 to 7");
+int DayNumber = Convert.ToInt32 (Console.ReadLine ()); 
+
+if (DayNumber > 7)
+{
+    System.Console.WriteLine("Error! Please enter the number from 1 to 7");
+    DayNumber = Convert.ToInt32 (Console.ReadLine ()); 
+}
+
+else if (DayNumber == 1)
+{
+    System.Console.WriteLine("Monday");
+}
+else if (DayNumber == 2)
+{
+    System.Console.WriteLine("Tuesday");
+}
+else if (DayNumber == 3)
+{
+    System.Console.WriteLine("Wednesday");
+}
+else if (DayNumber == 4)
+{
+    System.Console.WriteLine("Thursday");
+}
+else if (DayNumber == 5)
+{
+    System.Console.WriteLine("Friday");
+}
+else if (DayNumber == 6)
+{
+System.Console.WriteLine("Saturday");
+}
+else if (DayNumber == 7)
+{
+System.Console.WriteLine("Sunday");
+}
