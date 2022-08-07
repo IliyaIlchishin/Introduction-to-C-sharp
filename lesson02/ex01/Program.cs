1a4ff029fd701a5a449b2557ebfcc3320a2ec810
«iliya <iliya.91f@gmail.com>
2022-08-07 23:35:17 +0300

    lesson 2, ex01+ex02

--- lesson02/ex01/Program.cs
+++ lesson02/ex01/Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int Max(int arg1, int arg2, int arg3, int arg4)
+{
+    int result = 0;
+    if (arg1 > result) result = arg1;
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
+    if (arg4 > result) result = arg4;
+    return result;
+}
+
+
+int a = 123;
+int b = 223;
+int c = 425;
+int d = 14;
+int a1 = 435;
+int b1 = 913;
+int c1 = -235;
+int d1 = 38;
+int a2 = 42;
+int b2 = 123;
+int c2 = 97;
+int d2 = 432;
+int a3 = 57;
+int b3 = 326;
+int c3 = 1305;
+int d3 = 848;
+
+/*int max1 = Max(a, b, c, d);
+int max2 = Max(a1, b1, c1, d1);
+int max3 = Max(a2, b2, c2, d2);
+int max4 = Max(a3, b3, c3, d3);
+*/
+int max = Max (
+    Max(a1, b1, c1, d1),
+    Max(a, b, c, d),
+    Max(a2, b2, c2, d2),
+    Max(a3, b3, c3, d3));
+
+System.Console.WriteLine($"Максимальное число {max} ");
---
+++ lesson02/ex02/Program.cs
@@ -0,0 +1,20 @@
+﻿int Max(int arg1, int arg2, int arg3)
+{
+    int result = 0;
+    if (arg1 > result) result = arg1;
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
+
+    return result;
+}
+//               0    1   2   3    4   5   6   7   8     
+int[] array = { 41, 121, 31, 43, 11, 12, 44, 98, -55 };
+
+int result = Max (
+    Max (array[0],array[1],array[2]),
+    Max (array[3],array[4],array[5]),
+    Max (array[6],array[7],array[8])
+);
+
+
+System.Console.WriteLine(result);
