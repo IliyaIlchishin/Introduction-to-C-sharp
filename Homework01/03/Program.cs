b2fdc5aa190f9b8cd2363090b8b4c82843879469
«iliya <iliya.91f@gmail.com>
2022-08-07 11:04:20 +0300

    completed all task 01,02,03,04

--- Homework01/03/Program.cs
+++ Homework01/03/Program.cs
@@ -1,20 +1,17 @@
 ﻿
-System.Console.WriteLine("Please enter any number");
+System.Console.WriteLine("Введите число");
+
 int number = Convert.ToInt32 (Console.ReadLine());
-int value = number %2 ; 
+int value = number % 2 ; 
 
 if (value == 1)
+
 {
-System.Console.WriteLine($"The number {number} is odd"); 
-}
-else
-{
-    System.Console.WriteLine($"The number {number} is even"); 
+System.Console.WriteLine($"Число {number} нечетное"); 
 }
 
-/*
-Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
-4 -> да
--3 -> нет
-7 -> нет
-*/
+else
+{
+    System.Console.WriteLine($"Число {number} четное"); 
+}
+
---
+++ Homework01/04/Program.cs
@@ -0,0 +1,20 @@
+﻿System.Console.WriteLine("Please enter number ");
+int Number = Convert.ToInt32(Console.ReadLine());
+int value = 0; 
+
+
+
+while (value < Number)
+{
+    value++;
+
+    if (value % 2 == 0) {
+
+    System.Console.Write ($" {value} ");
+
+    }
+   
+
+}
+
+
