b00af8affc8a5c9ffdfc14bb8d0c6bb6153f10cb
«iliya <iliya.91f@gmail.com>
2022-08-07 23:55:19 +0300

    lesson 2

---
+++ lesson02/ex03/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = { 41, 121, 31, 43, 11, 12, 44, 31, -55 };
+
+int n = array.Length;
+int find = 31;
+int index = 0;
+
+while (index < n)
+{
+if (array[index] == find)
+{
+    System.Console.WriteLine(index);
+    break;
+}
+index++; 
+
+}
---
+++ lesson02/ex04/Program.cs
@@ -0,0 +1,13 @@
+﻿void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1,15);
+        index++;
+    }
+}
+
+int [] BigArray = {};
+System.Console.WriteLine(BigArray[3]);
