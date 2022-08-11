c3b19fd9397025e591a7dd4cb37b77814301b452
«iliya <iliya.91f@gmail.com>
2022-08-12 00:03:27 +0300

    home task 01,02

---
+++ Homework02/01/Program.cs
@@ -0,0 +1,14 @@
+﻿int NumberTrasformator(int num)
+
+{
+    int result = (num % 100) / 10;
+    return result;
+}
+
+int num1 = 456;
+int num2 = 782;
+int num3 = 918;
+
+int result = NumberTrasformator (num2);
+System.Console.WriteLine(result);
+
---
+++ Homework02/02/Program.cs
@@ -0,0 +1,40 @@
+﻿
+int NumberTrasformator(int num)
+
+{
+    int result = 0;
+
+    if (num > 99 )
+    {
+        result = num % 10;
+    }
+    if (num > 999 && num < 9999)
+    {
+        result = (num % 100) / 10;
+    }
+    if (num > 9999 && num < 99999)
+    {
+        result = (num % 1000) / 100;
+    }
+    else result = 999;
+
+    return result;
+}
+
+int num1 = 933;
+
+int result = NumberTrasformator(num1);
+System.Console.WriteLine($"Исходное число {num1}, третья цифра {result}");
+
+//if (result == 0) System.Console.WriteLine("Третьей цифры нет");
+//else System.Console.WriteLine($"Исходное число {num1}, третья цифра {result}");
+
+
+
+
+/*
+Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+645 -> 5
+78 -> третьей цифры нет
+32679 -> 6
+*/
