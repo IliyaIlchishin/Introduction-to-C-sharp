87ee5333f2c296588832c4ce884eb659641de076
«iliya <iliya.91f@gmail.com>
2022-08-12 23:18:01 +0300

    homework 01,02,03

---
+++ Homework02/03/Program.cs
@@ -0,0 +1,39 @@
+﻿
+int Weekday (int day)
+{
+    int result; 
+    if (day <= 5) 
+    {
+        result = 1;
+    }
+    else result = 2; 
+
+    return result;
+}
+
+int GetData() 
+{
+    Console.WriteLine("Пожалуйста введите день недели: ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+
+int weekday = Weekday (GetData()); 
+if (weekday == 1) System.Console.WriteLine("Нет, рабочий день");
+else System.Console.WriteLine("Да, выходной");
+
+
+/*  Как внутри функции результату присвоить стринговое значение? Выдает ошибку
+string Weekday (int day)
+{
+    string result; 
+    if (day <= 5) 
+    {
+        result = Convert.ToString (System.Console.WriteLine("Да, выходной"));
+    }
+    return result;
+}
+
+int weekday = Weekday (2); 
+System.Console.WriteLine(weekday);
+*/
