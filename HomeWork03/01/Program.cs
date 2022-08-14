8a0ad5aa186088c454da862415b6eb18f57b1f53
«iliya <iliya.91f@gmail.com>
2022-08-14 13:08:57 +0300

    new save

---
+++ HomeWork03/01/Program.cs
@@ -0,0 +1,47 @@
+﻿
+bool Palindrom(int[] array)
+
+{
+    int index1 = 0;
+    int index2 = 5;
+    bool result = false;
+   
+    if (array[index1] == array[index2])
+    {
+        System.Console.WriteLine($"{array [index1]}, {array [index2]}");
+        index1++; index2--;
+
+        System.Console.WriteLine($"{array [index1]}, {array [index2]}");
+        if (array[index1] == array[index2])
+        {
+            index1++; index2--;
+            System.Console.WriteLine($"{array [index1]}, {array [index2]}");
+
+            if (array[index1] == array[index2])
+            {
+                index1++; index2--;
+                System.Console.WriteLine($"{array [index1]}, {array [index2]}");
+                result = true;
+            } else result = false;
+
+        } else result = false;
+
+    } else result = false;
+
+    return result;
+
+};
+         
+int[] array = { 1, 3, 3, 3, 3, 1 };
+bool NumCheck = Palindrom(array);
+if (NumCheck == true) System.Console.WriteLine("The number is a Palindrom");
+else System.Console.WriteLine("The number is NOT a Palindrom");
+
+/*
+Задача 19
+Напишите метод, который принимает на вход шестизначное число и проверяет, является ли оно палиндромом.
+14212 -> нет
+12821 -> да
+23432 -> да
+строки использовать нельзя
+*/
---
+++ HomeWork03/02/Program.cs
@@ -0,0 +1,21 @@
+﻿
+double Coordinates (double[] a, double[] b)
+{
+
+double distance = Math.Pow (Math.Pow(a[0] - b[0], 2) +  Math.Pow(a[1]- b[1], 2) + Math.Pow(a[2] - b[2], 2), 0.5);
+
+return distance; 
+
+}
+
+double [] a = {3,6,8};
+double [] b = {2,1,-7};
+
+double [] c = {7,-5,0};
+double [] d = {1,-1,9};
+
+double distance = Coordinates (c, d );
+System.Console.WriteLine(distance);
+
+
+
